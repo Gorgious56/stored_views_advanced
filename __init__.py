70da6d80a1d509f8974724835aae0f552adc914c
Gorgious <nathan.hild@gmail.com>
2021-12-02 12:44:44 +0100

    Bump version

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "name": "Stored Views Advanced",
     "blender": (2, 93, 0),
     "category": "3D View",
-    "version": (0, 0, 1),
+    "version": (1, 0, 0),
     "author": "Gorgious56",
     "description": "Save and restore User defined views, pov, layers and display config",
 }
