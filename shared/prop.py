1f06a172ffd681a9eccd85a5223119cd1f0e4ca7
Gorgious <nathan.hild@gmail.com>
2021-12-06 19:07:46 +0100

    Improve deep copy

--- shared/prop.py
+++ shared/prop.py
@@ -1,6 +1,3 @@
-import bpy
-
-
 class PropsStore:
     def store(self, copy_from):
         for prop in self.__annotations__:
@@ -12,6 +9,9 @@ class PropsStore:
     def restore(self, copy_to):
         for prop in self.__annotations__:
             try:
-                setattr(copy_to, prop, getattr(self, prop))
+                copy_from_value = getattr(self, prop)
+                copy_to_value = getattr(copy_to, prop)
+                if copy_to_value != copy_from_value:
+                    setattr(copy_to, prop, copy_from_value)
             except AttributeError:
                 pass
