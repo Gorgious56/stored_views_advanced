94a46a785f0698873ea55e258673b22d93cdc3b2
Gorgious <nathan.hild@gmail.com>
2021-12-06 19:05:19 +0100

    Update collection status operators

--- collection/operator.py
+++ collection/operator.py
@@ -1,29 +1,43 @@
+
 import bpy
 from stored_views_advanced.outliner.helper import get_outliner_area
 from stored_views_advanced.view_layer.helper import get_family_down
 
 
-class SVA_OT_collections_store_or_restore_settings(bpy.types.Operator):
-    bl_label = "Store / Restore Collections Settings"
-    bl_idname = "sva.collections_store_or_restore_settings"
+def iterate_collections(context, container, index, parent, do):
+    collections_props = context.scene.sva.get_store(container).get(index).collections
+    collections_props_mapping = {c.name: c for c in collections_props}
+    for col in get_family_down(parent):
+        col_props = collections_props_mapping.get(col.name)
+        if col_props is None:
+            col_props = collections_props.add()
+            col_props.name = col.name
+        do(col_props, col)
 
-    index:  bpy.props.IntProperty()
-    store: bpy.props.BoolProperty()
+
+class SVA_OT_collections_store_settings(bpy.types.Operator):
+    bl_label = "Store Collections Settings"
+    bl_idname = "sva.collections_store_settings"
+
+    container: bpy.props.StringProperty()
+    index: bpy.props.IntProperty()
 
     def execute(self, context):
-        collections_props = context.scene.sva[self.index].collections
-        collections_props_mapping = {c.name: c for c in collections_props}
+        iterate_collections(
+            context, self.container, self.index, context.scene.collection, lambda props, col: props.store(col)
+        )
+        return {"FINISHED"}
 
-        for col in get_family_down(context.scene.collection):
-            col_props = collections_props_mapping.get(col.name)
-            if col_props is None:
-                col_props = collections_props.add()
-                col_props.name = col.name
-            if self.store:
-                col_props.store(col)
-            else:
-                col_props.restore(col)
+class SVA_OT_collections_restore_settings(bpy.types.Operator):
+    bl_label = "Restore Collections Settings"
+    bl_idname = "sva.collections_restore_settings"
 
+    container: bpy.props.StringProperty()
+    index: bpy.props.IntProperty()
+
+    def execute(self, context):
+        iterate_collections(
+            context, self.container, self.index, context.scene.collection, lambda props, col: props.restore(col)
+        )
         get_outliner_area(context).tag_redraw()
-
         return {"FINISHED"}
