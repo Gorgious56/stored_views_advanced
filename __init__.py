ebefa812574c770d10ac243fc30e8774391d0afb
Gorgious <nathan.hild@gmail.com>
2021-12-06 19:04:41 +0100

    remove dependency to stored_views addon

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from . import auto_load
-import bpy
 
 bl_info = {
     "name": "Stored Views Advanced",
@@ -18,9 +17,3 @@ def register():
 
 def unregister():
     auto_load.unregister()
-    from space_view3d_stored_views.operators import VIEW3D_stored_views_save
-    bpy.utils.register_class(VIEW3D_stored_views_save)
-    from space_view3d_stored_views.operators import VIEW3D_stored_views_set
-    bpy.utils.register_class(VIEW3D_stored_views_set)
-    from space_view3d_stored_views.operators import VIEW3D_stored_views_delete
-    bpy.utils.register_class(VIEW3D_stored_views_delete)
