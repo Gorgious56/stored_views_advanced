5bc881973faeca560ab9b50cbf0f505740e585d0
Gorgious <nathan.hild@gmail.com>
2021-12-08 13:14:23 +0100

    Cleanup view layer settings operator

--- view_layer/operator.py
+++ view_layer/operator.py
@@ -3,8 +3,8 @@ from stored_views_advanced.outliner.helper import get_outliner_area
 from stored_views_advanced.view_layer.helper import get_family_down
 
 
-def iterate_view_layers(context, container, index):
-    view_layers_props = context.scene.sva.get_store(container).get(index).view_layers
+def iterate_view_layers(context, store, index):
+    view_layers_props = context.scene.sva.get(store).get(index).view_layers
     view_layers_mapping = {vl.name: vl for vl in view_layers_props}
 
     for view_layer in context.scene.view_layers:
@@ -27,13 +27,12 @@ class SVA_OT_view_layers_store_settings(bpy.types.Operator):
     bl_label = "Store view Layer Settings"
     bl_idname = "sva.view_layers_store_settings"
 
-    container: bpy.props.StringProperty()
+    store: bpy.props.StringProperty()
     index: bpy.props.IntProperty()
 
     def execute(self, context):
-        for props, l_c in iterate_view_layers(context, self.container, self.index):
+        for props, l_c in iterate_view_layers(context, self.store, self.index):
             props.store(l_c)
-
         return {"FINISHED"}
 
 
@@ -41,34 +40,12 @@ class SVA_OT_view_layers_restore_settings(bpy.types.Operator):
     bl_label = "Restore view Layer Settings"
     bl_idname = "sva.view_layers_restore_settings"
 
-    container: bpy.props.StringProperty()
+    store: bpy.props.StringProperty()
     index: bpy.props.IntProperty()
 
     def execute(self, context):
-        for props, l_c in iterate_view_layers(context, self.container, self.index):
+        for props, l_c in iterate_view_layers(context, self.store, self.index):
             props.restore(l_c)
         get_outliner_area(context).tag_redraw()
-
-        return {"FINISHED"}
-
-
-class SVA_OT_view_layer_specific_store_or_restore_settings(bpy.types.Operator):
-    bl_label = "Store / Restore view Layer Settings"
-    bl_idname = "sva.view_layers_store_or_restore_settings"
-
-    name: bpy.props.IntProperty()
-    store: bpy.props.BoolProperty()
-
-    def execute(self, context):
-        view_layers_props = context.scene.sva.store_view_layer
-
-        try:
-            this_settings = next(e for e in view_layers_props if e.name == self.name)
-        except StopIteration:
-            if self.store:
-                this_settings = view_layers_props.add()
-
-        if self.store:
-            pass
 
         return {"FINISHED"}
