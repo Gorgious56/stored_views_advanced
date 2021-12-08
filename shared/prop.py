a30dd9eb6a96b8afff60d83ce69b14088b227033
Gorgious <nathan.hild@gmail.com>
2021-12-08 13:11:58 +0100

    Enable recursive settings management

--- shared/prop.py
+++ shared/prop.py
@@ -1,6 +1,14 @@
+import bpy
+
+
 class PropsStore:
     def store(self, copy_from):
         for prop in self.__annotations__:
+            if prop == "sub_props_store":
+                for sub_prop_name in getattr(self, prop):
+                    sub_prop = getattr(self, sub_prop_name.name)
+                    sub_prop.store(getattr(copy_from, sub_prop_name.name))
+                continue
             try:
                 setattr(self, prop, getattr(copy_from, prop))
             except AttributeError:
@@ -8,6 +16,11 @@ class PropsStore:
 
     def restore(self, copy_to):
         for prop in self.__annotations__:
+            if prop == "sub_props_store":
+                for sub_prop_name in getattr(self, prop):
+                    sub_prop = getattr(self, sub_prop_name.name)
+                    sub_prop.restore(getattr(copy_to, sub_prop_name.name))
+                continue
             try:
                 copy_from_value = getattr(self, prop)
                 copy_to_value = getattr(copy_to, prop)
@@ -15,3 +28,16 @@ class PropsStore:
                     setattr(copy_to, prop, copy_from_value)
             except AttributeError:
                 pass
+
+    def init(self):
+        if hasattr(self, "sub_props_store"):
+            self.sub_props_store.clear()
+            for prop_name in self.__annotations__:
+                prop = getattr(self, prop_name)
+                if isinstance(prop, PropsStore):
+                    new = self.sub_props_store.add()
+                    new.name = prop_name
+
+
+class StringPropertyGroup(bpy.types.PropertyGroup):
+    pass
